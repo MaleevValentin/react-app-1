d54031ee8a8c9725da8f506f982e7db29033de24
MaleevValentin <maleyev.valentin@gmail.com>
2020-06-26 19:05:43 +0300

    15 posts components, folder structure

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import s from './Post.module.css';
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img
+                src="https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+                alt=""/>
+            Post 1
+            <div>
+                <span>like</span>
+            </div>
+        </div>
+    );
+}
+
+export default Post;
