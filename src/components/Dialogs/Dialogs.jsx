6fbe6e7addc657c5038bffe5018088b9c0929374
MaleevValentin <maleyev.valentin@gmail.com>
2020-07-06 21:38:47 +0300

    21 page Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,33 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Gigi
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    David
+                </div>
+                <div className={s.dialog}>
+                    Mauro
+                </div>
+                <div className={s.dialog}>
+                    Paulo
+                </div>
+                <div className={s.dialog}>
+                    Juan
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Fine</div>
+                <div className={s.message}>Alla</div>
+                <div className={s.message}>Fine</div>
+            </div>
+        </div>
     );
 }
 
