38994812fe52dfcc126db8e273a00086b31ce1db
MaleevValentin <maleyev.valentin@gmail.com>
2020-07-08 22:34:29 +0300

    24 Business Logic Layer

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,20 +19,31 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Gigi'},
+        {id: 2, name: 'Alex'},
+        {id: 3, name: 'David'},
+        {id: 4, name: 'Mauro'},
+        {id: 5, name: 'Paulo'},
+        {id: 6, name: 'Juan'}
+    ];
+
+    let messagesData = [
+        {id: 1, message: 'Fine Alla FIne'},
+        {id: 2, message: 'Forza Juve'},
+        {id: 3, message: '!!!!!!!!!!'}
+    ];
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name='Gigi' id='1'/>
-                <DialogItem name='Alex' id='2'/>
-                <DialogItem name='David' id='3'/>
-                <DialogItem name='Mauro' id='4'/>
-                <DialogItem name='Paulo' id='5'/>
-                <DialogItem name='Juan' id='6'/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
             </div>
             <div className={s.messages}>
-                <Message message='Fine Alla FIne'/>
-                <Message message='Forza Juve'/>
-                <Message message='!!!!!!!!!!'/>
+                <Message message={messagesData[0].message} id={messagesData[0].id}/>
+                <Message message={messagesData[1].message} id={messagesData[1].id}/>
             </div>
         </div>
     );
