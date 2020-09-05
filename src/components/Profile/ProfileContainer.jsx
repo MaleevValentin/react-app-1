bd8153e310db33f00c50754d6b715a2635e861f8
MaleevValentin <maleyev.valentin@gmail.com>
2020-09-05 08:16:36 +0300

    71 setState, local state

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -31,4 +31,4 @@ let mapStateToProps = (state) => ({
 export default compose(
     connect(mapStateToProps, {getUserProfile}),
     withRouter,
-    withAuthRedirect)(ProfileContainer);
+    /*withAuthRedirect*/)(ProfileContainer);
