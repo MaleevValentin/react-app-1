3fed339a2ba874fff7ac3665cf42e89a8ee8c7fe
MaleevValentin <maleyev.valentin@gmail.com>
2020-10-15 23:35:38 +0300

    Updates

---
+++ precache-manifest.2d1eb69173ce0e4c41cf056fc5c4d0c9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05451391bfe348b2e54fe84f7a759c11",
+    "url": "/react-app-1/index.html"
+  },
+  {
+    "revision": "902a7f086f2c11bda70b",
+    "url": "/react-app-1/static/css/3.70aee952.chunk.css"
+  },
+  {
+    "revision": "110ec77f30a26ee5d57c",
+    "url": "/react-app-1/static/css/4.a7620e10.chunk.css"
+  },
+  {
+    "revision": "5fd45c309fcc7c132910",
+    "url": "/react-app-1/static/css/main.25f0635c.chunk.css"
+  },
+  {
+    "revision": "ff4fcd345944fd3a1d0e",
+    "url": "/react-app-1/static/js/2.17bcbbd2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-app-1/static/js/2.17bcbbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902a7f086f2c11bda70b",
+    "url": "/react-app-1/static/js/3.f9fccc29.chunk.js"
+  },
+  {
+    "revision": "110ec77f30a26ee5d57c",
+    "url": "/react-app-1/static/js/4.c39e82b8.chunk.js"
+  },
+  {
+    "revision": "5fd45c309fcc7c132910",
+    "url": "/react-app-1/static/js/main.4dc83f4e.chunk.js"
+  },
+  {
+    "revision": "9ad9758ce05689474e22",
+    "url": "/react-app-1/static/js/runtime-main.00304331.js"
+  },
+  {
+    "revision": "84a3c9c34e838ffa713e861fde6577f4",
+    "url": "/react-app-1/static/media/preloader.84a3c9c3.svg"
+  },
+  {
+    "revision": "f07d66e00150e23a0d8b2e10b7b0f86b",
+    "url": "/react-app-1/static/media/user.f07d66e0.png"
+  }
+]);
