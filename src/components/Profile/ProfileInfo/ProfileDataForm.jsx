29fc1d6428b498f88983a50fd3ea4df30ff731e1
MaleevValentin <maleyev.valentin@gmail.com>
2020-11-11 23:49:41 +0200

    97 update profile

---
+++ src/components/Profile/ProfileInfo/ProfileDataForm.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {CreateField, Input, Textarea} from "../../common/FormsControls/FormsControls";
+import {reduxForm} from "redux-form";
+import s from './ProfileInfo.module.css';
+import styles from "../../common/FormsControls/FormsControls.module.css";
+
+const ProfileDataForm = ({handleSubmit, profile, error}) => {
+    return (
+        <form onSubmit={handleSubmit}>
+
+            <div>
+                <button>Save</button>
+            </div>
+            {error && <div className={styles.formControlSummaryError}>
+                {error}
+            </div>}
+
+            <div>
+                <b>Full name</b>: {CreateField("Full name", "fullName", Input, [])}
+            </div>
+            <div>
+                <b>Looking for a job</b>: {CreateField("", "lookingForAJob", Input, [], {type: "checkbox"})}
+            </div>
+            <div>
+                <b>My professional skills</b>:
+                {CreateField("My professional skills", "lookingForAJobDescription", Textarea, [])}
+            </div>
+            <div>
+                <b>About me</b>:
+                {CreateField("About me", "aboutMe", Textarea, [])}
+            </div>
+            <div>
+                <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+                return <div key={key} className={s.contact}>
+                    <b>{key}: {CreateField(key, "contacts." + key, Input, [])}</b>
+                </div>
+            })}
+            </div>
+        </form>
+    )
+}
+
+const ProfileDataFormReduxForm = reduxForm({form: "edit-profile"})(ProfileDataForm);
+
+export default ProfileDataFormReduxForm;
